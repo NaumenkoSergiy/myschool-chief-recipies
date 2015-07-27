a844e87cd45fe16bd7b2183a5a4b772e6d36bcdc
NaumenkoSergiy <sergey.naumenko94@gmail.com>
2015-07-27 16:37:19 +0300

    change metadata for sidekiq

--- opsworks_sidekiq/metadata.rb
+++ opsworks_sidekiq/metadata.rb
@@ -3,3 +3,6 @@ maintainer 'BargeApp LLC'
 maintainer_email 'noah@bargeapp.com'
 license 'MIT'
 version '0.0.1'
+
+recipe "sidekiq::setup", "Setup Sidekiq"
+recipe "sidekiq::restart", "Restart Sidekiq"
