65c71905318a73772c1bf003723f78fb10423ba5
NaumenkoSergiy <sergey.naumenko94@gmail.com>
2015-07-28 15:42:30 +0300

    fix

--- sidekiq/recipes/deploy.rb
+++ sidekiq/recipes/deploy.rb
@@ -1,25 +1,25 @@
 node[:deploy].each do |application, deploy|
-  if deploy['sidekiq']
-    sidekiq_config = deploy['sidekiq']
-    release_path = ::File.join(deploy[:deploy_to], 'current')
-    start_command = sidekiq_config['start_command'] || "bundle exec sidekiq -C config/myschool_sidekiq.yml -d -L log/sidekiq.log"
-    env = deploy['environment_variables'] || {}
+  # if deploy['sidekiq']
+  #   sidekiq_config = deploy['sidekiq']
+  #   release_path = ::File.join(deploy[:deploy_to], 'current')
+  #   start_command = sidekiq_config['start_command'] || "bundle exec sidekiq -C config/myschool_sidekiq.yml -d -L log/sidekiq.log"
+  #   env = deploy['environment_variables'] || {}
 
-    template "setup sidekiq.conf" do
-      path "/etc/init/sidekiq-#{application}.conf"
-      source "sidekiq.conf.erb"
-      owner "root"
-      group "root"
-      mode 0644
-      variables({
-        app_name: application,
-        user: deploy[:user],
-        group: deploy[:group],
-        release_path: release_path,
-        start_command: start_command,
-        env: env,
-      })
-    end
+  #   template "setup sidekiq.conf" do
+  #     path "/etc/init/sidekiq-#{application}.conf"
+  #     source "sidekiq.conf.erb"
+  #     owner "root"
+  #     group "root"
+  #     mode 0644
+  #     variables({
+  #       app_name: application,
+  #       user: deploy[:user],
+  #       group: deploy[:group],
+  #       release_path: release_path,
+  #       start_command: start_command,
+  #       env: env,
+  #     })
+  #   end
 
     execute "start sidekiq on #{application}" do
       command "bundle exec sidekiq -C config/myschool_sidekiq.yml -d -L log/sidekiq.log"
@@ -35,5 +35,5 @@ node[:deploy].each do |application, deploy|
     #   code "echo noop"
     #   notifies :restart, "service[sidekiq-#{application}]"
     # end
-  end
+  # end
 end
