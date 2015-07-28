f3b8a62051dcd4a9872569d7387398a733456c06
NaumenkoSergiy <sergey.naumenko94@gmail.com>
2015-07-28 16:19:44 +0300

    test fix

--- sidekiq/recipes/deploy.rb
+++ sidekiq/recipes/deploy.rb
@@ -1,39 +1,48 @@
-node[:deploy].each do |application, deploy|
-  # if deploy['sidekiq']
-  #   sidekiq_config = deploy['sidekiq']
-  #   release_path = ::File.join(deploy[:deploy_to], 'current')
-  #   start_command = sidekiq_config['start_command'] || "bundle exec sidekiq -C config/myschool_sidekiq.yml -d -L log/sidekiq.log"
-  #   env = deploy['environment_variables'] || {}
+# node[:deploy].each do |application, deploy|
+#   # if deploy['sidekiq']
+#   #   sidekiq_config = deploy['sidekiq']
+#   #   release_path = ::File.join(deploy[:deploy_to], 'current')
+#   #   start_command = sidekiq_config['start_command'] || "bundle exec sidekiq -C config/myschool_sidekiq.yml -d -L log/sidekiq.log"
+#   #   env = deploy['environment_variables'] || {}
 
-  #   template "setup sidekiq.conf" do
-  #     path "/etc/init/sidekiq-#{application}.conf"
-  #     source "sidekiq.conf.erb"
-  #     owner "root"
-  #     group "root"
-  #     mode 0644
-  #     variables({
-  #       app_name: application,
-  #       user: deploy[:user],
-  #       group: deploy[:group],
-  #       release_path: release_path,
-  #       start_command: start_command,
-  #       env: env,
-  #     })
-  #   end
+#   #   template "setup sidekiq.conf" do
+#   #     path "/etc/init/sidekiq-#{application}.conf"
+#   #     source "sidekiq.conf.erb"
+#   #     owner "root"
+#   #     group "root"
+#   #     mode 0644
+#   #     variables({
+#   #       app_name: application,
+#   #       user: deploy[:user],
+#   #       group: deploy[:group],
+#   #       release_path: release_path,
+#   #       start_command: start_command,
+#   #       env: env,
+#   #     })
+#   #   end
 
-    execute "start sidekiq on #{application}" do
-      command "bundle exec sidekiq -C config/myschool_sidekiq.yml -d -L log/sidekiq.log"
-    end
+#     execute "start sidekiq on #{application}" do
+#       command "bundle exec sidekiq -C config/myschool_sidekiq.yml -d -L log/sidekiq.log"
+#     end
 
-    # service "sidekiq-#{application}" do
-    #   provider Chef::Provider::Service::Upstart
-    #   supports stop: true, start: true, restart: true, status: true
-    # end
+#     # service "sidekiq-#{application}" do
+#     #   provider Chef::Provider::Service::Upstart
+#     #   supports stop: true, start: true, restart: true, status: true
+#     # end
 
-    # # always restart sidekiq on deploy since we assume the code must need to be reloaded
-    # bash 'restart_sidekiq' do
-    #   code "echo noop"
-    #   notifies :restart, "service[sidekiq-#{application}]"
-    # end
-  # end
+#     # # always restart sidekiq on deploy since we assume the code must need to be reloaded
+#     # bash 'restart_sidekiq' do
+#     #   code "echo noop"
+#     #   notifies :restart, "service[sidekiq-#{application}]"
+#     # end
+#   # end
+# end
+
+rails_env = new_resource.environment["RAILS_ENV"]
+Chef::Log.info("test")
+
+execute "test" do
+  cwd release_path
+  command "bundle exec sidekiq -C config/myschool_sidekiq.yml -d -L log/sidekiq.log"
+  environment "RAILS_ENV" => rails_env
 end
