9e52a88427396049a2e786d185273ef886db80e4
NaumenkoSergiy <sergey.naumenko94@gmail.com>
2015-07-29 11:27:35 +0300

    fix

--- sidekiq/libraries/sidekiq.rb
+++ sidekiq/libraries/sidekiq.rb
@@ -1,7 +1,10 @@
 module OpsWorks
   module Sidekiq
     def self.kill_all_sidekiq
-      bash 'test' { code << "ps -ef | grep sidekiq | grep -v grep | awk '{print $2}' | xargs kill -9" }
+      bash 'test' do
+        code <<-EOH
+          (ps -ef | grep sidekiq | grep -v grep | awk '{print $2}' | xargs kill -9)
+        EOH
     end
   end
 end
