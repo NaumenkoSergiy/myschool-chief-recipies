5e66c62e148386b54929508b54e8ba4eefe5ee28
NaumenkoSergiy <sergey.naumenko94@gmail.com>
2015-07-29 10:46:24 +0300

    fix

--- sidekiq/recipes/start.rb
+++ sidekiq/recipes/start.rb
@@ -2,6 +2,11 @@ node[:deploy].each do |application, deploy|
   Chef::Log.info("test")
   release_path = ::File.join(deploy[:deploy_to], 'current')
 
+  file 'tmp/pids/sidekiq.pid' do
+    action :delete
+    backup false
+  end
+
   execute "start" do
     cwd release_path
     command "bundle exec sidekiq -C config/myschool_sidekiq.yml -d -L log/sidekiq.log -p tmp/pids/sidekiq.pid"
