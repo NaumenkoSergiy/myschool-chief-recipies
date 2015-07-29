8ff927f3a672251b628203a1a1178c46fedfbd21
NaumenkoSergiy <sergey.naumenko94@gmail.com>
2015-07-29 10:16:09 +0300

    fix

--- sidekiq/recipes/start.rb
+++ sidekiq/recipes/start.rb
@@ -3,9 +3,9 @@ node[:deploy].each do |application, deploy|
   release_path = ::File.join(deploy[:deploy_to], 'current')
 
   # execute "start" do
-  cwd release_path
     # OpsWorks::Sidekiq.kill_all_sidekiq
   code <<-EOH
+  (cd #{release_path})
   (pkill -f sidekiq)
   (bundle exec sidekiq -C config/myschool_sidekiq.yml -d -L log/sidekiq.log)
   EOH
