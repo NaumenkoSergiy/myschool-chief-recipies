61773874f8442472b007a9c3873b5d0921181639
NaumenkoSergiy <sergey.naumenko94@gmail.com>
2015-07-29 16:03:30 +0300

    fix

--- sidekiq/recipes/start.rb
+++ sidekiq/recipes/start.rb
@@ -13,6 +13,6 @@ node[:deploy].each do |application, deploy|
     cwd release_path
     # command "pkill -f sidekiq; bundle exec sidekiq -C config/myschool_sidekiq.yml -d -L log/sidekiq.log"
     # environment "RAILS_ENV" => 'staging'
-    command "deploy.sh start #{release_path}"
+    command "/opt/aws/opsworks/current/site-cookbooks/sidekiq/recipes/deploy.sh start #{release_path}"
   end
 end
