141b1505af6f727b1c78c01e875b3c89be7df642
NaumenkoSergiy <sergey.naumenko94@gmail.com>
2015-07-29 09:34:35 +0300

    fix

--- sidekiq/recipes/start.rb
+++ sidekiq/recipes/start.rb
@@ -41,8 +41,7 @@ node[:deploy].each do |application, deploy|
 
   execute "start" do
     cwd release_path
-    command "pkill -f sidekiq; bundle exec sidekiq -C config/myschool_sidekiq.yml -d -L log/sidekiq.log"
-    action :run
+    command "bundle exec sidekiq -C config/myschool_sidekiq.yml -d -L log/sidekiq.log"
     environment "RAILS_ENV" => 'staging'
   end
 end
