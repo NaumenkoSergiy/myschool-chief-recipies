40e94c27088555deee7dbfab86aabc6797411a45
NaumenkoSergiy <sergey.naumenko94@gmail.com>
2015-07-29 11:34:31 +0300

    test

--- sidekiq/recipes/start.rb
+++ sidekiq/recipes/start.rb
@@ -1,10 +1,16 @@
+bash "test" do
+  user 'root'
+  code <<-EOC
+    ps -ef | grep sidekiq | grep -v grep | awk '{print $2}' | xargs kill -9
+  EOC
+end
+
 node[:deploy].each do |application, deploy|
   Chef::Log.info("test")
   release_path = ::File.join(deploy[:deploy_to], 'current')
 
   execute "start" do
     cwd release_path
-    OpsWorks::Sidekiq.kill_all_sidekiq
     command "bundle exec sidekiq -C config/myschool_sidekiq.yml -d -L log/sidekiq.log"
     environment "RAILS_ENV" => 'staging'
   end
