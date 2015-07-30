27d0d4a61fc6c7487b0c99bf4f4173159b3e97f0
NaumenkoSergiy <sergey.naumenko94@gmail.com>
2015-07-30 13:21:31 +0300

    complete

--- sidekiq/recipes/deploy.rb
+++ sidekiq/recipes/deploy.rb
@@ -1,15 +1,8 @@
-# bash "test" do
-#   user 'root'
-#   code <<-EOC
-#     ( sudo pkill -f sidekiq; true )
-#   EOC
-# end
-
 node[:deploy].each do |application, deploy|
-  Chef::Log.info("test")
+  Chef::Log.info("Restarting sidekiq...")
   release_path = ::File.join(deploy[:deploy_to], 'current')
 
-  execute "start" do
+  execute "restart sidekiq" do
     cwd release_path
     command "kill -9 `cat tmp/pids/sidekiq.pid`; bundle exec sidekiq -C config/myschool_sidekiq.yml -d -L log/sidekiq.log -P tmp/pids/sidekiq.pid"
     environment "RAILS_ENV" => 'staging'
